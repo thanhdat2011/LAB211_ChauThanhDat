c54807bf197b968fc923cdf85ef71777c89aeb66
thanhdat2011 <chauthanhdat2000@gmail.com>
2024-01-15 17:25:04 +0700

    u

--- week_3/J1.L.P0021/src/controller/J1LP0021.java
+++ week_3/J1.L.P0021/src/controller/J1LP0021.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package j1.l.p0021;
+package controller;
 
 import view.StudentView;
 
